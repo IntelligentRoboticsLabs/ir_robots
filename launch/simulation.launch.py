4d29e68eecc628109fa05f01a624ca17e4640536
jmguerreroh <josemiguel.guerrero@urjc.es>
2023-03-03 12:15:38 +0100

    Adding TIAGo's arm option

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -18,12 +18,17 @@ import os
 from os import environ, pathsep
 import yaml
 
-from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from ament_index_python.packages import (get_package_share_directory,
+                                         get_package_prefix)
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument
+from launch.actions import (IncludeLaunchDescription, SetEnvironmentVariable,
+                            DeclareLaunchArgument)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_pal.include_utils import include_launch_py_description
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def get_model_paths(packages_names):
@@ -67,11 +72,13 @@ def generate_launch_description():
     simulation = conf['ir_robots']['simulation']
 
     if not simulation:
-        print("Simulation param is set to false, please change it to run the simulator: " + config)
+        print("Simulation param is set to false, \
+              please change it to run the simulator: " + config)
         return LaunchDescription()
 
+    arm = conf['ir_robots']['tiago_arm']
     arm_arg = DeclareLaunchArgument(
-        'arm', default_value='no-arm',
+        'arm', default_value=arm,
         description='Tiago arm'
     )
 
@@ -100,28 +107,32 @@ def generate_launch_description():
         if "hospital" in world_name:
             gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('aws_robomaker_hospital_world'),
+                    get_package_share_directory(
+                        'aws_robomaker_hospital_world'),
                     'launch'), '/view_hospital.launch.py']),
             )
         # Racetrack - default day. Change mode in /view_racetrack.launch.py
         if "racetrack" in world_name:
             gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('aws_robomaker_racetrack_world'),
+                    get_package_share_directory(
+                        'aws_robomaker_racetrack_world'),
                     'launch'), '/view_racetrack.launch.py']),
             )
         # Small warehouse
         if "warehouse" in world_name:
             gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('aws_robomaker_small_warehouse_world'),
+                    get_package_share_directory(
+                        'aws_robomaker_small_warehouse_world'),
                     'launch'), '/no_roof_small_warehouse.launch.py']),
             )
         # Bookstore
         if "bookstore" in world_name:
             gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('aws_robomaker_bookstore_world'),
+                    get_package_share_directory(
+                        'aws_robomaker_bookstore_world'),
                     'launch'), '/view_bookstore.launch.py']),
             )
 
@@ -144,7 +155,8 @@ def generate_launch_description():
     # @TODO: pal_pcl_points_throttle_and_filter
 
     packages = ['tiago_description', 'pmb2_description',
-                'hey5_description', 'pal_gripper_description', 'kobuki_description']
+                'hey5_description', 'pal_gripper_description',
+                'kobuki_description']
     model_path = get_model_paths(packages)
     resource_path = get_resource_paths(packages)
 
@@ -154,20 +166,37 @@ def generate_launch_description():
     if 'GAZEBO_RESOURCE_PATH' in environ:
         resource_path += pathsep + environ['GAZEBO_RESOURCE_PATH']
 
+    moveit_arg = DeclareLaunchArgument(
+        'moveit', default_value='false',
+        description='Specify if launching MoveIt2'
+    )
+    move_group = include_launch_py_description(
+        'tiago_moveit_config', ['launch', 'move_group.launch.py'],
+        condition=IfCondition(LaunchConfiguration('moveit')))
+
+    tuck_arm = Node(package='tiago_gazebo',
+                    executable='tuck_arm.py',
+                    emulate_tty=True,
+                    output='both')
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
     ld.add_action(SetEnvironmentVariable('GAZEBO_MODEL_PATH', model_path))
     # Using this prevents shared library from being found
-    # ld.add_action(SetEnvironmentVariable('GAZEBO_RESOURCE_PATH', tiago_resource_path))
-    ld.add_action(arm_arg)
+    # ld.add_action(SetEnvironmentVariable('GAZEBO_RESOURCE_PATH',
+    #                                       tiago_resource_path))
     ld.add_action(world_name_arg)
     ld.add_action(gazebo)
 
     robot_name = conf['ir_robots']['robot']
     if 'tiago' in robot_name:
+        ld.add_action(arm_arg)
         ld.add_action(tiago_spawn)
         ld.add_action(tiago_bringup)
+        ld.add_action(moveit_arg)
+        ld.add_action(move_group)
+        ld.add_action(tuck_arm)
     elif 'kobuki' in robot_name:
         ld.add_action(kobuki_spawn)
     else:
