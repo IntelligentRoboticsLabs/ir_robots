18ec1767e7f060f9b5f46c1c12faba73fb79cf22
Juancams <jc.manzanares.serrano@gmail.com>
2023-03-21 09:57:46 +0100

    Error with tf

--- launch/dependencies/robot_spawn.launch.py
+++ launch/dependencies/robot_spawn.launch.py
@@ -111,7 +111,7 @@ def generate_launch_description():
                                  executable='static_transform_publisher',
                                  output='screen',
                                  arguments=['0.0', '0.0', '0.0',
-                                            '-1.56', '0.0', '-1.56',
+                                            '0.0', '0.0', '0.0',
                                             'base_link',
                                             'base_footprint'])
 
